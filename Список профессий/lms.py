58abfb7b4cd3c5daef89b4b930caa3b7382df7ad
diossi <151754955+diossi@users.noreply.github.com>
2024-03-06 20:02:25 +0400

    Add files via upload

---
+++ Список профессий/lms.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+list_prof = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач']
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='mars one')
+
+
+@app.route('/training/engineers')
+def training_engineers():
+    return render_template('training.html', title='mars one', prof='engineers')
+
+
+@app.route('/training/scientists')
+def training_scientists():
+    return render_template('training.html', title='mars one', prof='scientists')
+
+
+@app.route('/list_prof')
+def list_prof_def():
+    return render_template('list_prof.html', list='ul', list_prof=list_prof)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+  
