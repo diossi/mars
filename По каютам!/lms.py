23197ce5bc70561494cc16c4d98232bf78e477f2
diossi <151754955+diossi@users.noreply.github.com>
2024-03-14 15:13:28 +0400

    Add files via upload

---
+++ По каютам!/lms.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+global_form = None
+list_members = ['1', '2', 'asdasd', 'zxc', 'qwe']
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='mars one')
+
+
+@app.route('/training/<prof>')
+def training_engineers(prof):
+    return render_template('training.html', title='mars one', prof=prof)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global global_form
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        global_form = form
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html', title='mars one', form=global_form)
+
+
+@app.route('/distribution')
+def distribution():
+    return render_template('distribution.html', title='mars one', list_members=list_members)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+  
